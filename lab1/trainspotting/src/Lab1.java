4c981ba48bea7e6f45668e2e95960bb158fd8cf4
Meyer <Qcube.gaming@gmail.com>
2025-01-24 11:47:01 +0100

    Added sensors but fails

--- lab1/trainspotting/src/Lab1.java
+++ lab1/trainspotting/src/Lab1.java
@@ -1,12 +1,24 @@
 import TSim.*;
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.concurrent.Semaphore;
 
 public class Lab1 {
 
   public Lab1(int speed1, int speed2) {
 
     // Create two instances of train controller
-    TrainBrain brain1 = new TrainBrain(1, speed1);
-    TrainBrain brain2 = new TrainBrain(2, speed2);
+    HashMap<Point,Semaphore> semaphores = new HashMap<>();
+    // Add 5 semaphores, each representing a critical section of the rail
+    Semaphore intersection1 = new Semaphore(1);
+    semaphores.put(new Point(6, 7),intersection1);
+    semaphores.put(new Point(10, 7),intersection1);
+    semaphores.put(new Point(8, 5),intersection1);
+    semaphores.put(new Point(10, 8),intersection1);
+
+
+    TrainBrain brain1 = new TrainBrain(1, speed1, semaphores);
+    TrainBrain brain2 = new TrainBrain(2, speed2, semaphores);
     
     // Create two threads which keep track of one train each
     Thread train1 = new Thread(brain1);
@@ -20,10 +32,13 @@ public class Lab1 {
   private class TrainBrain implements Runnable {
     
     private int trainid, initialSpeed;
+    private Semaphore intersection1;
+    private HashMap<Point, Semaphore> shared_railway;
 
-    public TrainBrain(int trainid, int initialSpeed) {
+    public TrainBrain(int trainid, int initialSpeed, HashMap<Point, Semaphore> semaphores ) {
       this.trainid = trainid;
       this.initialSpeed = initialSpeed;
+      this.shared_railway = semaphores;
     }
 
 
@@ -34,14 +49,20 @@ public class Lab1 {
 
         try {
           tsi.setSpeed(trainid, initialSpeed);
-          tsi.setSpeed(trainid, 0);
-          
-          tsi.getSensor(1);
+          tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+          while (true) { 
+            SensorEvent event = tsi.getSensor(trainid);
+            Semaphore semaphore = shared_railway.get(new Point(event.getXpos(), event.getYpos()));
+            if(!semaphore.tryAcquire()){
+              //Nothing
+              tsi.setSpeed(trainid, 0);
+            }
+          }  
         }
 
         catch (CommandException e) {
           e.printStackTrace();
-        } 
+        }
         catch (InterruptedException e) {
           e.printStackTrace();
         }
