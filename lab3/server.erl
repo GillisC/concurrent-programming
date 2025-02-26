7801899ab87b906c26d457d3e874b2cee4e76ee2
Meyer <Qcube.gaming@gmail.com>
2025-02-26 15:15:36 +0100

    Added basic setup

--- lab3/server.erl
+++ lab3/server.erl
@@ -1,18 +1,27 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1, messageLoop/0]).%,stop/1]).
+
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
+    Pid = spawn(?MODULE, messageLoop, []),
+    Pid ! ServerAtom.
     % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    not_implemented.
-
+    % not_implemented.
+ 
 % Stop the server process registered to the given name,
 % together with any other associated processes
-stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+% stop(ServerAtom) ->
+%     % TODO Implement function
+%     % Return ok
+%     not_implemented.
+
+messageLoop() ->
+    receive 
+        started -> io:format("Server started!");
+        _ -> io:format("HI")
+    end.
