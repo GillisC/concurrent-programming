2eb4bea26035fd2b5293bce098d3e3e690dfdf54
Meyer <Qcube.gaming@gmail.com>
2025-03-03 11:28:16 +0100

    Refactor channel module and client module
    - Refactor the channel module to use a function reference in the `start` function.
    - Add a new function `handle/2` to the `channel` module.
    - Modify the `client` module to print the server state and make a request to join a channel.

--- lab3/channel.erl
+++ lab3/channel.erl
@@ -1,21 +1,23 @@
 -module(channel).
--export([start/1]).
+-export([stop/1, start/1, handle/2]).
 
 
 start(ChannelAtom) ->
-    Pid = genserver:start(ChannelAtom, [], handle),
+    Pid = genserver:start(ChannelAtom, [], fun handle/2),
     Pid. 
 
 stop(ChannelAtom) ->
     genserver:stop(ChannelAtom).
 
 handle(Users, {join, Client}) ->
+    io:format("~p~n", [here]),
     case lists:member(Client, Users) of 
         true  -> 
-            {reply, ok, Client}
+            {reply, ok, Client};
         false -> 
             {reply, ok, [Client | Users]}
     end. 
+    
 
     
     
--- lab3/server.erl
+++ lab3/server.erl
@@ -6,7 +6,7 @@
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    Pid = genserver:start(ServerAtom, [], handle),
+    Pid = genserver:start(ServerAtom, [], fun handle/2),
     Pid. 
  
 % Stop the server process registered to the given name,
@@ -16,14 +16,15 @@ stop(ServerAtom) ->
 
 % This is F we send into genserver
 handle(Channels, {join, ChannelAtom, Client}) ->
+    io:format("~p~n", [Channels]),
     NewState = case lists:member(ChannelAtom, Channels) of 
         false -> 
-            channel:start(ChannelAtom),     
+            channel:start(list_to_atom(ChannelAtom)),     
             [ChannelAtom | Channels];
         true -> 
             Channels
     end,
-    Result = genserver:request(ChannelAtom, {join, Client}),
+    Result = genserver:request(list_to_atom(ChannelAtom), {join, Client}),
     {reply, Result, NewState}.
 
                  
