7adb1b370501e37c3b2c5d6319cb6c904b8b2dc8
Meyer <Qcube.gaming@gmail.com>
2025-03-10 12:34:54 +0100

    Refactor channel module to improve message handling and user management

--- lab3/channel.erl
+++ lab3/channel.erl
@@ -25,20 +25,15 @@ handle(Users, {leave, Client}) ->
             {reply, ok, lists:delete(Client, Users)} %Make this quit 
     end;
 
-handle(Users, {message_send, Msg, Client, Nick}) ->
-    io:format("Client: ~p~n", [Client]),
-    io:format("message: ~p~n", [Msg]),
-    io:format("Nick: ~p~n", [Nick]),
-    io:format("Users: ~p~n", [lists:delete(Client, Users)]),
-    io:format("Users2: ~p~n", [Users]),
+handle(Users, {message_send, Channel, Msg, Client, Nick}) ->
     case lists:member(Client, Users) of
         false ->
             {reply, {error, user_not_joined, "User not a member!"}, Users};
         true ->
-        lists:foreach(fun(X) -> genserver:request(X, {message_receive, get_string_from_pid() , Nick, Msg}) end, 
+        lists:foreach(fun(X) -> genserver:request(X, {message_receive, Channel, Nick, Msg}) end, 
                         lists:delete(Client, Users)),
         {reply, ok, Users}    
     end.
 
-get_string_from_pid() ->
-    atom_to_list(element(2,process_info(self(), registered_name))).
+% get_string_from_pid() -> This was pro coding but took to long for 500ms
+%     atom_to_list(element(2,process_info(self(), registered_name))).
--- lab3/client.erl
+++ lab3/client.erl
@@ -44,8 +44,14 @@ handle(St, {leave, Channel}) ->
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    Result = genserver:request(list_to_atom(Channel), {message_send, Msg, self(), St#client_st.nick}),
-    {reply, Result, St};
+    case catch genserver:request(list_to_atom(Channel), {message_send, Channel, Msg, self(), St#client_st.nick}) of
+        {'EXIT', _} ->
+            {reply, {error, server_not_reached, "Channel failed!"}, St};
+        timeout_error -> 
+            {reply, {error, server_not_reached, "Channel not reached!"}, St};
+        Result ->
+            {reply, Result, St}
+    end;
 
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
@@ -62,12 +68,9 @@ handle(St, whoami) ->
 
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
-    case catch gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}) of
-        {'EXIT', _} ->
-            {reply, {error, server_not_reached}, St};
-        Result -> 
-            {reply, Result, St}
-    end;
+    io:format("Client: ~p~n", [{Channel, Nick, Msg}]),
+    gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}),
+    {reply, ok, St} ;
 
 % Quit client via GUI
 handle(St, quit) ->
